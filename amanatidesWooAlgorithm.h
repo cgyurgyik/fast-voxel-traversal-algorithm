d97cd639939cfbdac0c4783543352200a4d8427d
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-02-12 19:18:18 -0500

    Update comment.

--- amanatidesWooAlgorithm.h
+++ amanatidesWooAlgorithm.h
@@ -6,8 +6,9 @@
 
 // Provides a basis for Amanatides & Woo's "A Fast Voxel Traversal Algorithm for Ray Tracing."
 // See: https://www.researchgate.net/publication/2611491_A_Fast_Voxel_Traversal_Algorithm_for_Ray_Tracing
-// If the ray origin is outside the voxel grid, uses an updated version of Smit's algorithm to determine intersection.
-// [t0, t1] determine the begin and end time for which the ray travels.
+// If the ray origin is outside the voxel grid, uses a safer version of Smit's ray box intersection algorithm
+// to determine intersection. The bounds [t0, t1] determine the begin and end time for which the ray travels.
+//
 // Notes: Assumes that indices for voxel coordinates begin at 1.
 void amanatidesWooAlgorithm(const Ray& ray, const Grid3D& grid, value_type t0, value_type t1) noexcept;
 
